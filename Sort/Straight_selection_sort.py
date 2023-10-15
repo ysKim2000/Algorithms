c77606fb2a5d4fc675fea23f1d9b2ce51252b87e
Yunseo <86720575+ysKim2000@users.noreply.github.com>
2023-10-16 00:33:26 +0900

    Straight selection sort

---
+++ Sort/Straight_selection_sort.py
@@ -0,0 +1,9 @@
+def selectionSort(x):
+	length = len(x)
+	for i in range(length-1):
+	    indexMin = i
+		for j in range(i+1, length):
+			if x[indexMin] > x[j]:
+				indexMin = j
+		x[i], x[indexMin] = x[indexMin], x[i]
+	return x
